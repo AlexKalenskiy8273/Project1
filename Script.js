a9f4cf574619f30fbb270fa954059b5ed9055679
AlexKalenskiy8273 <51775561+AlexKalenskiy8273@users.noreply.github.com>
2019-06-13 09:16:27 +0300

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,7 @@
+function chItem(){
+	document.getElementByClassName('Mercury-cont').style.animationPlayState = 'paused';
+}
+
+function rchItem(){
+	document.getElementByClassName('Mercury-cont').style.animationPlayState = 'running';
+}
